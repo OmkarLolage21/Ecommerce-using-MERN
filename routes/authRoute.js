c95c2cf067a675397d679cfa6c259a2e88fe78c9
Omkar Sunil Lolage <lolageomkar5@gmail.com>
2024-03-19 21:58:23 +0530

    Full stack commit

---
+++ routes/authRoute.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import {testController, loginController,registerController,forgotPasswordController,updateProfileController, getOrdersController} from '../controllers/authController.js';
+import {isAdmin, requireSignIn} from '../middlewares/authMiddleware.js';
+
+const router= express.Router();
+// REGISTER || POST
+router.post('/register',registerController);
+
+//LOGIN || POST
+router.post('/login',loginController);
+
+//FORGOT PASSWORD || POST
+router.post('/forgot-password',forgotPasswordController)
+
+//test route
+router.get('/test',requireSignIn,isAdmin, testController);
+
+//protected route
+router.get('/user-auth', requireSignIn, (req,res)=>{
+    res.status(200).send({ok:true});
+})
+
+router.get('/admin-auth', requireSignIn, isAdmin, (req,res)=>{
+    res.status(200).send({ok:true});
+});
+
+router.put('/profile',requireSignIn,updateProfileController);
+
+router.get('/orders',requireSignIn,getOrdersController);
+export default router;
